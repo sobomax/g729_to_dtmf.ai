9b0e81157db9547d1c1b4ff1621df97a157c91d3
Maksym Sobolyev <sobomax@sippysoft.com>
2021-02-18 18:24:00 -0800

    Also build/install libsndfile.

--- scripts/do-install_depends.sh
+++ scripts/do-install_depends.sh
@@ -6,7 +6,7 @@ BASEDIR="`dirname "${0}"`/.."
 #. "${BASEDIR}/scripts/functions.sub"
 
 BCG729_VER=1.1.1
-#SNDFILE_VER=1.0.28
+SNDFILE_VER=1.0.28
 
 uname -a
 which ${CC}
@@ -18,12 +18,21 @@ autoreconf --version
 
 mkdir deps
 cd deps
+
 wget -O bcg729-${BCG729_VER}.tar.gz \
   https://github.com/BelledonneCommunications/bcg729/archive/${BCG729_VER}.tar.gz
 tar xfz bcg729-${BCG729_VER}.tar.gz
 cd bcg729-${BCG729_VER}
 touch NEWS AUTHORS ChangeLog bcg729.spec.in # Hello, automake! :)
 ./autogen.sh
+./configure
+make
+sudo make install
+cd -
+
+wget http://www.mega-nerd.com/libsndfile/files/libsndfile-${SNDFILE_VER}.tar.gz
+tar xfz libsndfile-${SNDFILE_VER}.tar.gz
+cd libsndfile-${SNDFILE_VER}
 ./configure
 make
 sudo make install
