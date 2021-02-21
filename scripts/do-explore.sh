2f8387e8daf04480b63bd27e54898c1a1d052bd5
Maksym Sobolyev <sobomax@sippysoft.com>
2021-02-21 09:52:42 -0800

    Generate PNG.

--- scripts/do-explore.sh
+++ scripts/do-explore.sh
@@ -12,4 +12,6 @@ do
   ofname="${ofpref}.sln"
   sox "${infile}" --bits 16 --encoding signed-integer --endian little "${ofname}"
   makeann "${ofname}" "${ofpref}"
+  g729_ofname="${ofpref}.18"
+  ${PYTHON_CMD} -m pipenv run ${PYTHON_CMD} read_g729_data.py "${g729_ofname}"
 done
