926e61dacbbf32716eaabc89d4ed42b523d2162a
Maksym Sobolyev <sobomax@sippysoft.com>
2021-02-24 13:24:22 -0800

    Generate all volume variations from 10% to 100% in 10% increments. This is to give training data generator some inputs to work with.

--- scripts/do-explore.sh
+++ scripts/do-explore.sh
@@ -7,11 +7,14 @@ BASEDIR="`dirname "${0}"`/.."
 
 for infile in ${BASEDIR}/training_set/*.wav
 do
-  ifname="`basename "${infile}"`"
-  ofpref="${ifname%.wav}"
-  ofname="${ofpref}.sln"
-  sox "${infile}" --bits 16 --encoding signed-integer --endian little "${ofname}"
-  makeann "${ofname}" "${ofpref}"
-  g729_ofname="${ofpref}.18"
-  ${PYTHON_CMD} -m pipenv run ${PYTHON_CMD} read_g729_data.py "${g729_ofname}"
+  for volume in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
+  do
+    ifname="`basename "${infile}"`"
+    ofpref="${ifname%.wav}.vol${volume}"
+    ofname="${ofpref}.sln"
+    sox --volume ${volume} "${infile}" --bits 16 --encoding signed-integer --endian little "${ofname}"
+    makeann "${ofname}" "${ofpref}"
+    g729_ofname="${ofpref}.18"
+    ${PYTHON_CMD} -m pipenv run ${PYTHON_CMD} read_g729_data.py "${g729_ofname}"
+  done
 done
